11f9381633850cb63343ee8f4d9f0f084faa8a92
Rogério Alves <rogerio.alves651@gmail.com>
2025-05-07 15:53:45 -0300

    Aprimorar script de desinstalação remota: adicionar listagem de programas instalados e melhorar mensagens de erro.

--- Desinstalação de Programas/BAT/desinstalador.bat
+++ Desinstalação de Programas/BAT/desinstalador.bat
@@ -4,21 +4,25 @@
 set /p remoteComputer=Digite o nome ou IP do computador remoto: 
 
 :: Solicita o nome do programa a ser desinstalado
-set /p programName=Digite o nome do programa a ser desinstalado: 
+set /p programName=Digite o nome do programa a ser desinstalado (exatamente como aparece no painel de controle): 
 
 :: Caminho do PsExec
 set "psexecPath=C:\PSTools\PsExec.exe"  :: Substitua pelo caminho do PsExec
 
+:: Comando para listar programas no computador remoto (para depuração)
+echo Listando programas instalados no computador remoto "%remoteComputer%"...
+%psexecPath% \\%remoteComputer% cmd /c "wmic product get name"
+
 :: Comando para localizar e desinstalar o programa no computador remoto
-set "wmicCommand=wmic product where ""name like '%%%programName%%%'" call uninstall /nointeractive"
+set "wmicCommand=wmic product where ""name='%programName%'"" call uninstall /nointeractive"
 
 :: Executa o comando remoto usando PsExec
 echo Iniciando desinstalação do programa "%programName%" no computador remoto "%remoteComputer%"...
-%psexecPath% \\%remoteComputer% cmd /c %wmicCommand%
+%psexecPath% \\%remoteComputer% cmd /c "%wmicCommand%"
 
 if %errorlevel% equ 0 (
     echo Comando enviado. Verifique se o programa foi desinstalado com sucesso.
 ) else (
-    echo Erro ao tentar desinstalar o programa.
+    echo Erro ao tentar desinstalar o programa. Verifique se o nome do programa está correto e tente novamente.
 )
 pause
